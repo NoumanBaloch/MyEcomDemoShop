1366965720e6e5ef8d5cde01edc52043c20ce3da
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-03-03 23:41:26 +0500

    Adding Entity Framework and Inital Initial DB Migrations

---
+++ MyEcomDemoShop/MyEcomDemoShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using MyEcomDemoShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEcomDemoShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("myEcomShopDB")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public  DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
