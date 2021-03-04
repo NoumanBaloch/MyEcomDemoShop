b9672412bd22409979c6f0d38e98723d3f8b64cb
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-03-04 23:50:09 +0500

    SQL Repository Implementation

--- MyEcomDemoShop/MyEcomDemoShop.WebUI/App_Start/UnityConfig.cs
+++ MyEcomDemoShop/MyEcomDemoShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyEcomDemoShop.Core.Contracts;
 using MyEcomDemoShop.Core.Models;
 using MyEcomDemoShop.DataAccess.InMemory;
+using MyEcomDemoShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyEcomDemoShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         
         }
     }
