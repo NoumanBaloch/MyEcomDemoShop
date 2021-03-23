92bd8da6dd22963e49063148cfd5d84340fd5a68
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-03-23 23:25:42 +0500

    Creating Product Filter

---
+++ MyEcomDemoShop/MyEcomDemoShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyEcomDemoShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEcomDemoShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
